19d4766580d448dba58ea29a1263a2dae3ca6f71
Vinicius Paim <viniciuspaimdacosta@yahoo.com.br>
2024-07-08 10:47:32 -0300

    Author data projection with DTO.

--- src/main/java/com/viniciuspaim/workshopmongo/config/Instantiation.java
+++ src/main/java/com/viniciuspaim/workshopmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.viniciuspaim.workshopmongo.config;
 
+import com.viniciuspaim.workshopmongo.DTO.AuthorDTO;
 import com.viniciuspaim.workshopmongo.domain.Post;
 import com.viniciuspaim.workshopmongo.domain.User;
 import com.viniciuspaim.workshopmongo.repository.PostRepository;
@@ -33,10 +34,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem","Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
