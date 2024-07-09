572bb7fc34b699752609fb4c79f492916219b9d8
Vinicius Paim <viniciuspaimdacosta@yahoo.com.br>
2024-07-09 14:19:14 -0300

    Search with multiple params

--- src/main/java/com/viniciuspaim/workshopmongo/resources/util/URL.java
+++ src/main/java/com/viniciuspaim/workshopmongo/resources/util/URL.java
@@ -3,9 +3,27 @@ package com.viniciuspaim.workshopmongo.resources.util;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
     public static String decodeParam(String text) {
-        return URLDecoder.decode(text, StandardCharsets.UTF_8);
+        try{
+            return URLDecoder.decode(text, "UTF-8");
+        }catch (UnsupportedEncodingException e){
+            return "";
+        }
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
     }
 }
